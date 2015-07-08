b7df0e569a795e21653004c77d8af2430d0144d1
james <jsaucier@sfu.ca>
2015-07-08 14:24:56 -0700

    refactored Interpreter

--- src/sfu/cmpt307/handleInput/Interpreter.java
+++ src/sfu/cmpt307/handleInput/Interpreter.java
@@ -12,20 +12,32 @@ public class Interpreter {
 	}
 
 	public void run() {
-		TwoThreeTree tree = initializeTree();
+		TwoThreeTree tree = initializeTreeElements();
 		runOperations(tree);
 	}
 
-	private TwoThreeTree initializeTree() {
+	private TwoThreeTree initializeTreeElements() {
 		if (scanner.getInitalTreeElements().size() < 2) {
 			throw new IllegalArgumentException(
 					"Tree must be initialized with 2 or more elements");
 		}
-
-		// Manually initializing the Two-Three tree with the first two input
-		// elements.
-		// Having a tree with only one element would break the properties of a
-		// Two-Three tree that the Insert(), Delete() operations depend on
+		
+		TwoThreeTree tree = manuallyInitTwoNodes();
+		for (int i = 2; i < scanner.getInitalTreeElements().size(); i++) {
+			tree.insert(scanner.getInitalTreeElements().get(i));
+		}
+		return tree;
+	}
+	
+	private void runOperations(TwoThreeTree tree) {
+		
+	}
+	
+	// Manually initializing the Two-Three tree with the first two input
+	// elements.
+	// Having a tree with only one element would break the properties of a
+	// Two-Three tree that the Insert(), Delete() operations depend on
+	private TwoThreeTree manuallyInitTwoNodes() {
 		TwoThreeNode root = new TwoThreeNode();
 		TwoThreeNode one = new TwoThreeNode(scanner.getInitalTreeElements()
 				.get(0));
@@ -37,7 +49,4 @@ public class Interpreter {
 		return new TwoThreeTree(root);
 	}
 	
-	private void runOperations(TwoThreeTree tree) {
-		
-	}
 }
